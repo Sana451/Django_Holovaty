f33a5368023b80c6fdc9bcc35fc397e8289e0845
sana451 <sana451@mail.ru>
2022-11-08 16:30:26 +0300

    Выполнена настройка базы данных mySQL.

--- mysite/holovaty_site/urls.py
+++ mysite/holovaty_site/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from holovaty_site.views import hello, my_homepage_view, current_datetime, hours_ahead
+from holovaty_site.views import hello, my_homepage_view, current_datetime, hours_ahead, sqlite3_connect, mySQLdb_connect
 
 urlpatterns = [
     re_path(r"^time/$", current_datetime),
@@ -8,4 +8,6 @@ urlpatterns = [
     path(r"", my_homepage_view),
     re_path(r"^time/plus/(\d{1,2})/$", hours_ahead),
     re_path(r"^time/$", current_datetime),
+    re_path(r"sqlite", sqlite3_connect),
+    re_path(r"mysql", mySQLdb_connect),
 ]
--- mysite/holovaty_site/views.py
+++ mysite/holovaty_site/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse, Http404
 import datetime
 from django.shortcuts import render
+import sqlite3
+import pyodbc
+import MySQLdb
+from django.db import connection
 
 
 def my_homepage_view(request):
@@ -25,3 +29,31 @@ def hours_ahead(request, offset):
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
     print(locals())
     return render(request, "hours_ahead.html", {"hour_offset": offset, "next_time": dt})
+
+
+def sqlite3_connect(request):
+    sqlite_connection = sqlite3.connect(":memory:")
+    sqlite_connection.execute("CREATE TABLE lang(name, first_appeared)")
+    data = [
+        ("C++", 1985),
+        ("Objective-C", 1984),
+    ]
+    sqlite_connection.executemany("INSERT INTO lang(name, first_appeared) VALUES(?, ?)", data)
+    print("Success connect")
+    cursor = sqlite_connection.cursor()
+    sqlite_select_query = "SELECT * FROM lang"
+    cursor.execute(sqlite_select_query)
+    records = cursor.fetchall()
+    print(type(records))
+    cursor.close()
+    sqlite_connection.close()
+    return render(request, "templ.html", {"records": records})
+
+
+def mySQLdb_connect(request):
+    cur = connection.cursor()
+    select_query = "SELECT * FROM city"
+    cur.execute(select_query)
+    records = cur.fetchall()
+    cur.close()
+    return render(request, "templ.html", {"records": records})
