5ee219d8f4d8b76cc9b0f31a8489914fce0916df
sana451 <sana451@mail.ru>
2022-11-10 05:51:18 +0300

    Зарегистрирован административный сайт.

--- mysite/holovaty_site/urls.py
+++ mysite/holovaty_site/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, re_path
 from holovaty_site.views import hello, my_homepage_view, current_datetime, hours_ahead, sqlite3_connect, mySQLdb_connect
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     re_path(r"^time/$", current_datetime),
     re_path(r'^hello/$', hello),
-    # re_path(r'^$', my_homepage_view),
-    path(r"", my_homepage_view),
+    path(r"", my_homepage_view),  # re_path(r'^$', my_homepage_view),
     re_path(r"^time/plus/(\d{1,2})/$", hours_ahead),
     re_path(r"^time/$", current_datetime),
     re_path(r"sqlite", sqlite3_connect),
